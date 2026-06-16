a8f6548cab792c03783bee0c6e2bef368570cfd6
maybuck <jmayer99@gmail.com>
2026-06-16 13:38:18 +0500

    fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,7 +11,7 @@ export class AppController {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Welcome - TechBox</title>
+        <title>Saas Dashboard</title>
         <style>
           body {
             margin: 0;
@@ -60,6 +60,7 @@ export class AppController {
       <body>
         <div class="card">
           <h1>Welcome to Code Blue Saas BackEnd</h1>
+          <p>Your all-in-one solution for managing and tracking cases.</p>
         </div>
       </body>
       </html>
