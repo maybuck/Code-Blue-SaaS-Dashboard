a7b328cb3740a73011e89e845bf5c1f03578d131
maybuck <jmayer99@gmail.com>
2026-06-16 12:55:01 +0500

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CasesModule } from './cases/cases.module';
 import { PermissionsModule } from './permissions/permissions.module';
-import { DriveModule } from './drive/drive.module';
-import { MediaModule } from './media/media.module';
+// import { DriveModule } from './drive/drive.module';
+// import { MediaModule } from './media/media.module';
 import { AgenciesModule } from './agencies/agencies.module';
 import { StatusesModule } from './statuses/statuses.module';
 
--- src/cases/cases.module.ts
+++ src/cases/cases.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { CasesService } from './cases.service';
 import { CasesController } from './cases.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { DriveModule } from 'src/drive/drive.module';
+// import { DriveModule } from 'src/drive/drive.module';
 
 @Module({
-  imports: [DriveModule],
+  // imports: [DriveModule],
   controllers: [CasesController],
   providers: [CasesService, PrismaService],
 })
